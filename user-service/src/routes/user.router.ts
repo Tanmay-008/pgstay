e826b26b698922a91a0bdd0c2eec2bd9cb09bcda
Tanmay-008 <tanmayshirbhayye60@gmail.com>
2026-07-06 01:40:25 +0530

    add controller

---
+++ user-service/src/routes/user.router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { login, register } from '../controller/user.controller';
+export const router = Router();
+
+
+router.post("login", login);
+router.post("register", register);
