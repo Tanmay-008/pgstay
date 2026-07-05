edca75ebf42664a28adfcaf89509c437f329c252
Tanmay-008 <tanmayshirbhayye60@gmail.com>
2026-07-06 00:18:03 +0530

    add user model

---
+++ user-service/src/core/model/user. model.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from "mongoose";
+
+const user = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    },
+
+})
+
+export const userSchema = model("userSchema", user)
