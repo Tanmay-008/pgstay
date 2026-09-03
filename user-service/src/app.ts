4a5330fe906a7fc84d03fe3926e043e8ce24a045
Tanmay-008 <tanmayshirbhayye60@gmail.com>
2026-09-04 00:07:32 +0530

    add cors origin in the backend service

--- user-service/src/app.ts
+++ user-service/src/app.ts
@@ -3,20 +3,19 @@ import cookieParser from "cookie-parser";
 import cors from "cors"
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
-        `${process.env.FRONTEND_URL}`,
+        "https://pgstay.tanmayshirbhayye.tech",
     ],
     credentials: true
 }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-console.log("server is start")
 
 import { router } from "./routes/user.router";
 import { errorMiddleware } from "./middleware/error.middleware";
